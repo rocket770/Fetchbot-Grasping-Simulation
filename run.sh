d65fc50c69366872262137ac8d8ba508624c12c9
rocket770 <60917649+rocket770@users.noreply.github.com>
2023-10-30 13:09:13 +1100

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,12 +1,11 @@
-#!/bin/bash
-source ~/catkin_ws/devel/setup.bash
-gnome-terminal -- bash -c "roslaunch fetch_gazebo pickplace_playground.launch; exec bash"
+!/bin/bash
+gnome-terminal -- bash -c "source ~/catkin_ws/devel/setup.bash && roslaunch fetch_pickplace pickplace_playground.launch; exec bash"
 
-sleep 1;
-source ~/catkin_ws/devel/setup.bash
+sleep 2;
+
 gnome-terminal -- bash -c "roslaunch fetch_moveit_config move_group.launch; exec bash"
 
-sleep 1;
+sleep 2;
 
 source ~/catkin_ws/devel/setup.bash
 gnome-terminal -- bash -c "rosrun matlab_moveit_bridge /src/bridge_node.py; exec bash"
